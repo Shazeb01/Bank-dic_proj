b1bc6d320a9758f868cdf360528985473597b2b3
Sc0rpion2 <shazebhussaini1994@gmail.com>
2020-09-28 17:10:08 +0530

    add, update

--- main.go
+++ main.go
@@ -3,12 +3,21 @@ package main
 import (
 	"fmt"
 
-	"github.com/Shazeb01/Bank-dic_proj/pkg/accounts"
+	"github.com/Shazeb01/Bank-dic_proj/pkg/mydict"
 )
 
 func main() {
-	account := accounts.NewAccount("nico")
-	account.Deposit(100)
-	fmt.Println(account)
-
+	dictionary := mydict.Dictionary{}
+	word := "hello"
+	definition := "Greeting"
+	err := dictionary.Add("hello", "Greeting")
+	if err != nil {
+		fmt.Println(err)
+	}
+	hello, _ := dictionary.Search(word)
+	fmt.Println("found", word, "definition:", hello)
+	err2 := dictionary.Add(word, definition)
+	if err2 != nil {
+		fmt.Println(err2)
+	}
 }
---
+++ pkg/mydict/mydict.go
@@ -0,0 +1,43 @@
+package mydict
+
+import "errors"
+
+//Dictionary type
+type Dictionary map[string]string
+
+var errNotFound = errors.New("Not Found")
+var errCantUpdate = errors.New("Can't update")
+var errWordExists = errors.New("exists")
+
+//Search word
+func (d Dictionary) Search(word string) (string, error) {
+	value, exists := d[word]
+	if exists {
+		return value, nil
+	}
+	return "", errNotFound
+}
+
+//Add word
+func (d Dictionary) Add(word, def string) error {
+	_, err := d.Search(word)
+	switch err {
+	case errNotFound:
+		d[word] = def
+	case nil:
+		return errWordExists
+	}
+	return nil
+}
+
+//Update words
+func (d Dictionary) Update(word, definition string) error {
+	_, err := d.Search(word)
+	switch err {
+	case nil:
+		d[word] = definition
+	case errNotFound:
+		return errCantUpdate
+	}
+	return nil
+}
